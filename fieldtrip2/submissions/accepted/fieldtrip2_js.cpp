9a4da060433ce5e5181d8593ffe14960046d94f1
Johan Sannemo <jsannemo@google.com>
2019-05-23 23:42:25 +0200

    [fieldtrip2] fixed solution

--- fieldtrip2/submissions/accepted/fieldtrip2_js.cpp
+++ fieldtrip2/submissions/accepted/fieldtrip2_js.cpp
@@ -15,61 +15,40 @@ int main() {
 
   int N, K;
   cin >> N >> K;
-  // Cannot have non-friend on the bus if K = 1
-  if (K == 1) {
-    cout << "0 0" << endl;
-    return 0;
-  }
 
   vi groups(N);
   rep(i,0,N) cin >> groups[i];
+  sort(all(groups));
+  reverse(all(groups));
 
-  // We cannot include any groups of size 1
-  rep(i,0,sz(groups)) {
-    if (groups[i] == 1) {
-      swap(groups[i], groups.back());
-      groups.pop_back();
-      --i;
+  ll sum = accumulate(all(groups), 0LL);
+  if (sum / K == 0) {
+    cout << "0 0" << endl;
+  } else if (sum / K == 1) {
+    int rem = 0;
+    int left = K;
+    trav(it, groups) {
+      left -= max(1, it - 1);
     }
+    trav(it, groups) {
+      if (left <= 0) break;
+      left--;
+      rem += it;
+    }
+    cout << 1 << " " << rem << endl;
+  } else {
+    ll remove = sum % K;
+    ll mx = groups.back();
+    
+    mx -= remove;
+    sum -= remove;
+
+    // OK if (mx - Y) <= (sum - mx + Y) * (K - 1)
+    // (mx - Y) * (1 + K -1) <= sum * (K - 1)
+    // (mx - Y) * K <= sum * (K - 1)
+    // mx * K - sum * (K - 1) <= YK
+    // mx - sum * (K - 1) / K <= Y
+    ll changes = max(0LL, mx - sum * (K - 1) / K);
+    cout << sum / K << " " << changes << endl;
   }
-  N = (int)groups.size();
-  int sum = accumulate(all(groups), 0);
-
-  // If we have a number of friends not divisible by K, we need to remove enough to be
-  // divisible by K
-  multiset<int> sorted;
-  rep(i,0,N) sorted.insert(groups[i]);
-  while (sum % K) {
-    auto it = --sorted.end();
-    int group = *it;
-    sorted.erase(it);
-    group--;
-    if (group) {
-      sorted.insert(group);
-    }
-    --sum;
-
-    // We may have created a 1-group doing this -- they need to be removed.
-    if (sorted.size()) {
-      if (*sorted.begin() == 1) {
-        sorted.erase(sorted.begin());
-        --sum;
-      }
-    }
-  }
-
-  // Now we can pack everyone left on buses greedily, unless the largest
-  // group X fullfills
-  // X > (sum - X) * (K - 1)
-  // X > (K - 1)sum - (K - 1)X
-  // XK > (K - 1)sum
-  //
-  // If we convert Y candies, we need to have that
-  // (X - Y)K <= (K - 1)sum
-  // So that
-  // (X - Y) <= (K - 1)sum / K
-  ll mxAfter = ((K - 1) * sum + K - 1) / K;
-  ll newKind = max(0LL, *sorted.rbegin() - mxAfter);
-
-  cout << sum / K << " " << newKind << endl;
 }
