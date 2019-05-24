40f639433b695cc931265171b3e982e63e869fc8
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-24 22:12:59 +0200

    [squatting] extend alphabet to a-z0-9

--- squatting/input_format_validators/validator/validator.cpp
+++ squatting/input_format_validators/validator/validator.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 void run() {
-	long long maxsum = Arg("s");
-	int n = Int(1, Arg("n"));
+	int maxsum = Arg("s");
+	int maxlen = Arg("len", maxsum);
+	int n = Int(1, maxsum);
 	Endl();
 	long long sum = 0;
 	for (int i = 0; i < n; i++) {
+		int len = 0;
 		for (;;) {
 			char c = Char();
 			if (c == '\n') break;
-			assert('a' <= c && c <= 'z');
+			assert(('a' <= c && c <= 'z') || ('0' <= c && c <= '9'));
 			sum++;
+			len++;
 		}
+		assert(len <= maxlen);
 	}
 	assert(sum <= maxsum);
 }
