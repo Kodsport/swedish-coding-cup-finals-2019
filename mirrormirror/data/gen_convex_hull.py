fe2296219efb9d7afbbc007480290ec4653e10f5
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-25 01:51:53 +0200

    [mirrormirror] test data improvements

--- mirrormirror/data/gen_convex_hull.py
+++ mirrormirror/data/gen_convex_hull.py
@@ -12,7 +12,7 @@ def right(a, b, p):
 
 # generate convex hull
 def graham_scan(pointList):
-    bottomPoint = (30000, 0)
+    bottomPoint = (30001, 0)
     for p in pointList:
         bottomPoint = min(bottomPoint, (p[1], p[0]))
     bottomPoint = (bottomPoint[1], bottomPoint[0])
@@ -39,24 +39,42 @@ def graham_scan(pointList):
 
 n    = int(cmdlinearg('n'))
 maxx = int(cmdlinearg('maxx'))
+fact = float(cmdlinearg('fact'))
+wobble = int(cmdlinearg('wobble'))
+odd = int(cmdlinearg('odd', 0))
+rand = int(cmdlinearg('rand', 1))
 
-genN = 400*n
+genN = int(fact*n)
 points = set()
 
 centerX = float(random.randint(-maxx//300, maxx//300))
 centerY = float(random.randint(-maxx//300, maxx//300))
-radius = float(random.randint(maxx - 1000, maxx - 1))
+radius = float(random.randint(maxx - maxx//300 - 1000, maxx - maxx//300 - 1))
+
+if odd:
+    oddPoint = (round(centerX), round(centerY + radius))
+    points.add(oddPoint)
 
 for i in range(genN):
     angle = random.uniform(0, 2*math.pi)
-    x = int(radius*math.cos(angle) + centerX)
-    y = int(radius*math.sin(angle) + centerY)
+    x = round((radius - random.uniform(0, wobble)) * math.cos(angle) + centerX)
+    y = round((radius - random.uniform(0, wobble)) * math.sin(angle) + centerY)
     points.add((x,y))
+    if odd:
+        points.add((2*round(centerX)-x,y))
 
-hullPoints = graham_scan(list(points))
+hullPoints = graham_scan(sorted(list(points)))
+
+if rand:
+    random.shuffle(hullPoints)
+if odd and oddPoint in hullPoints:
+    ind = hullPoints.index(oddPoint)
+    hullPoints[0], hullPoints[ind] = hullPoints[ind], hullPoints[0]
 
 while len(hullPoints) > n:
     hullPoints.pop()
+
+random.shuffle(hullPoints)
 
 print(len(hullPoints))
 for (x, y) in hullPoints:
--- mirrormirror/data/generator.sh
+++ mirrormirror/data/generator.sh
@@ -15,7 +15,7 @@ compile gen_answer_is_barely_3.cpp opt
 # Antal punkter
 MAXN=1500
 
-# Högsta absolutbeloppet av en koordinat (|xi| <= MAXX && |yi| <= MAXX)
+# Högsta absolutbeloppet av en koordinat (|x_i|, |y_i| <= MAXX)
 MAXX=30000
 
 samplegroup
@@ -24,37 +24,37 @@ sample 1
 
 group group1 10
 limits nMax=20 xMax=$MAXX
-tc smalln0 gen_random n=2   maxx=5
-tc smalln1 gen_random n=3   maxx=10  
-tc smalln2 gen_random n=10  maxx=10
-tc smalln3 gen_random n=20  maxx=10
-tc smalln4 gen_random n=20  maxx=$((MAXX/2))
-tc smalln5 gen_random n=20  maxx=$MAXX
-tc smalln6 gen_line   n=10  maxx=$MAXX        lines=1
-tc smalln7 gen_line   n=20  maxx=$MAXX        lines=2
-tc smalln8 gen_line   n=20  maxx=$MAXX        lines=3
-tc smalln9 gen_random n=1   maxx=$MAXX
-tc smalln10 gen_answer_is_barely_3 20 40
+tc 1-n1 gen_random n=1   maxx=$MAXX
+tc 1-n2 gen_random n=2   maxx=5
+tc 1-n3 gen_random n=3   maxx=10  
+tc 1-smalln2 gen_random n=10  maxx=10
+tc 1-smalln3 gen_random n=20  maxx=10
+tc 1-smalln4 gen_random n=20  maxx=$((MAXX/2))
+tc 1-smalln5 gen_random n=20  maxx=$MAXX
+tc 1-smalln6 gen_line   n=10  maxx=$MAXX        lines=1
+tc 1-smalln7 gen_line   n=20  maxx=$MAXX        lines=2
+tc 1-smalln8 gen_line   n=20  maxx=$MAXX        lines=3
+tc 1-smalln9 gen_answer_is_barely_3 20 40
 
 group group2 7
 limits nMax=800 xMax=$MAXX nocolinear=1
 # TODO
-tc nocolinear1 gen_random n=800  maxx=$MAXX
+tc 2-nocolinear1 gen_random n=800  maxx=$MAXX
 
 group group3 8
 limits nMax=800 xMax=$MAXX
 include_group group2
 include_group group1
-tc mediumn0 gen_random n=800  maxx=$MAXX
-tc mediumn1 gen_random n=800  maxx=$MAXX
-tc mediumn2 gen_random n=800  maxx=$MAXX
-tc mediumn3 gen_random n=800  maxx=$MAXX
-tc mediumn4 gen_random n=800  maxx=$MAXX
-tc mediumn5 gen_random n=800  maxx=$MAXX
-tc mediumn6 gen_line n=800  maxx=$MAXX   lines=1
-tc mediumn7 gen_line n=800  maxx=$MAXX   lines=3
-tc mediumn8 gen_line n=800  maxx=$MAXX   lines=10
-tc mediumn9 gen_answer_is_barely_3 800 $MAXX
+tc 3-mediumn0 gen_random n=800  maxx=$MAXX
+tc 3-mediumn1 gen_random n=800  maxx=$MAXX
+tc 3-mediumn2 gen_random n=800  maxx=$MAXX
+tc 3-mediumn3 gen_random n=800  maxx=$MAXX
+tc 3-mediumn4 gen_random n=800  maxx=$MAXX
+tc 3-mediumn5 gen_random n=800  maxx=$MAXX
+tc 3-mediumn6 gen_line n=800  maxx=$MAXX   lines=1
+tc 3-mediumn7 gen_line n=800  maxx=$MAXX   lines=3
+tc 3-mediumn8 gen_line n=800  maxx=$MAXX   lines=10
+tc 3-mediumn9 gen_answer_is_barely_3 800 $MAXX
 
 group group4 10
 limits nMax=$MAXN xMax=$MAXX
@@ -62,29 +62,37 @@ limits nMax=$MAXN xMax=$MAXX
 
 group group5 15
 limits nMax=$MAXN xMax=$MAXX convexhull=1
-tc bign_convex0 gen_convex_hull n=$MAXN  maxx=$MAXX
-tc bign_convex1 gen_convex_hull n=$MAXN  maxx=$MAXX
-tc bign_convex2 gen_convex_hull n=$MAXN  maxx=$MAXX
-tc bign_convex3 gen_convex_hull n=$MAXN  maxx=$MAXX
-tc bign_convex4 gen_convex_hull n=$MAXN  maxx=$MAXX
-tc bign_convex5 gen_convex_hull n=$MAXN  maxx=$MAXX
+tc 5-bign_convex0 gen_convex_hull n=$MAXN  maxx=$MAXX fact=1   wobble=0
+tc 5-bign_convex1 gen_convex_hull n=$MAXN  maxx=$MAXX fact=400 wobble=2
+tc 5-bign_convex2 gen_convex_hull n=$MAXN  maxx=$MAXX fact=20  wobble=5
+tc 5-bign_convex3 gen_convex_hull n=$MAXN  maxx=$MAXX fact=400 wobble=0
+tc 5-bign_convex4 gen_convex_hull n=$MAXN  maxx=$MAXX fact=400 wobble=0
+tc 5-bign_convex5 gen_convex_hull n=$MAXN  maxx=$MAXX fact=400 wobble=0 rand=0
+tc 5-bign_convex6 gen_convex_hull n=$MAXN  maxx=$MAXX fact=2   wobble=2 odd=1 seed=128
+tc 1-n1
+tc 1-n2
+tc 1-n3
 
 group group6 25
 limits nMax=$MAXN xMax=$MAXX
 include_group group5
 include_group group3
-tc bign0 gen_answer_is_barely_3 $MAXN $MAXX
-tc bign1 gen_random n=$MAXN  maxx=$MAXX
-tc bign2 gen_random n=$MAXN  maxx=$MAXX
-tc bign3 gen_random n=$MAXN  maxx=$MAXX
-tc bign4 gen_random n=$MAXN  maxx=$MAXX
-tc bign5 gen_random n=$MAXN  maxx=$MAXX
-tc bign6 gen_random n=$MAXN  maxx=$MAXX
-tc bign7 gen_line n=$MAXN  maxx=$MAXX    lines=1
-tc bign8 gen_line n=$MAXN  maxx=$MAXX    lines=3
-tc bign9 gen_line n=$MAXN  maxx=$MAXX    lines=10
-tc bign10 gen_random n=$MAXN  maxx=32 # The smallest square that fits 4000 points
-tc bign11 gen_answer_is_2 $MAXN $MAXX
+tc 6-bign1 gen_random n=$MAXN  maxx=$MAXX
+tc 6-bign2 gen_random n=$MAXN  maxx=$MAXX
+tc 6-bign3 gen_random n=$MAXN  maxx=$MAXX
+tc 6-bign4 gen_random n=$MAXN  maxx=$MAXX
+tc 6-bign5 gen_random n=$MAXN  maxx=$MAXX
+tc 6-bign6 gen_random n=$MAXN  maxx=$MAXX
+tc 6-bign7 gen_line n=$MAXN  maxx=$MAXX    lines=1
+tc 6-bign8 gen_line n=$MAXN  maxx=$MAXX    lines=3
+tc 6-bign9 gen_line n=$MAXN  maxx=$MAXX    lines=10
+tc 6-bign10 gen_random n=$MAXN  maxx=32 # The smallest square that fits 4000 points
+tc 6-a-barely3-1 gen_answer_is_barely_3 $MAXN $MAXX
+tc 6-a-barely3-2 gen_answer_is_barely_3 $MAXN $MAXX
+tc 6-a-is2-1 gen_answer_is_2 $MAXN $MAXX
+tc 6-c-barely3-3 gen_answer_is_barely_3 $MAXN $MAXX
+tc 6-c-barely3-4 gen_answer_is_barely_3 $MAXN $MAXX
+tc 6-c-is2-2 gen_answer_is_2 $MAXN $MAXX
 
 wait
 for A in secret/group6/*.in; do
