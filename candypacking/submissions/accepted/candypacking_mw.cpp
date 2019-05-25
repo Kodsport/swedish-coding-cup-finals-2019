bd47c0579bfee07c14d6d95874fde0de0f79a8df
Mårten Wiman <marten.wiman@gmail.com>
2019-05-25 09:37:52 +0200

    [candypacking] Another solution.

---
+++ candypacking/submissions/accepted/candypacking_mw.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = (a); i < int(b); ++i)
+#define rrep(i, a, b) for(int i = (a) - 1; i >= int(b); --i)
+#define trav(it, v) for(auto &it : (v))
+#define all(v) (v).begin(), (v).end()
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define sz(x) (int)(x).size()
+
+typedef double fl;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+
+int main(){
+	ios::sync_with_stdio(0);
+    ll M, K;
+    cin >> M >> K;
+    vector<ll> a(M);
+    rep(i,0,M) {
+        cin >> a[i];
+    }
+    ll maxA = *max_element(all(a));
+    ll sumA = accumulate(all(a), 0L);
+    ll boxes = sumA/K;
+    ll modified = 0;
+    if (boxes == 1) {
+        ll sum = 0;
+        rep(i,0,M) {
+            sum += min(K-1, max(1LL, a[i]-1));
+        }
+        if (sum < K) {
+            modified = K - sum;
+        }
+    }
+    else if (sumA - maxA < boxes) {
+        modified = boxes - (sumA - maxA);
+    }
+    cout << boxes << " " << modified << endl;
+}
